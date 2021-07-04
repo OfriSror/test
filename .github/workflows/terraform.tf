3346bd45b95286ce0bac9a9c6884ba436de15acb
OfriSror <ofri@lightlytics.com>
2021-07-04 12:35:07 +0300

    Add design file

--- .github/workflows/terraform.tf
+++ .github/workflows/terraform.tf
@@ -1,9 +1,3 @@
-resource "aws_instance" "my-instance" {
-  ami           = "ami-0ddbdea833a8d2f0d"
-  instance_type = "t2.micro"
-  
-  tags = {
-    Name = "my-instance"
-    ManagedBy = "Terraform"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
